6d98e74a56312961a130e21d598a180ed25527b2
ajaykushwah17 <123883271+ajaykushwah17@users.noreply.github.com>
2023-10-13 16:34:46 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "AZURE_CLIENT_ID" {
+  description = "Azure Client ID"
+}
+
+variable "AZURE_SECRET" {
+  description = "Azure Client Secret"
+}
+
+variable "AZURE_SUBSCRIPTION_ID" {
+  description = "Azure Subscription ID"
+}
+
+variable "AZURE_TENANT_ID" {
+  description = "Azure Tenant ID"
+}
