495e09dd44a60ccf6f2863e381605e5e24085f61
ajaykushwah17 <123883271+ajaykushwah17@users.noreply.github.com>
2023-10-13 13:57:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,65 +1,6 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "myResourceGroup"
-  location = "East US"
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "myVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "myPublicIP"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-}
-
-resource "azurerm_network_security_group" "example" {
-  name     = "myNetworkSecurityGroup"
-  location = azurerm_resource_group.example.location
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "myNIC"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_virtual_machine" "example" {
   name                  = "myVM"
-  location              = azurerm_resource_group.example.location
+  location              = "East US"
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.example.id]
   vm_size               = "Standard_DS1_v2"
@@ -92,7 +33,7 @@ resource "azurerm_virtual_machine" "example" {
   }
 
   provisioner "file" {
-    source      = "https://raw.githubusercontent.com/ajaykushwah17/Terraform-Ansible/main/path/to/your/playbook.yml"
+    source      = "https://github.com/ajaykushwah17/Terraform-Ansible"
     destination = "/tmp/playbook.yml"
   }
 
