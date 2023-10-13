cae401e9352abf57481820c9e8d479660a17f8ca
ajaykushwah17 <123883271+ajaykushwah17@users.noreply.github.com>
2023-10-13 14:03:02 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,44 +1,86 @@
-resource "azurerm_virtual_machine" "example" {
-  name                  = "myVM"
-  location              = "East US"
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS1_v2"
+provider "azurerm" {
+  features {}
 
-  storage_os_disk {
-    name              = "myOsDisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
+  client_id       = var.AZURE_CLIENT_ID
+  client_secret   = var.AZURE_SECRET
+  subscription_id = var.AZURE_SUBSCRIPTION_ID
+  tenant_id       = var.AZURE_TENANT_ID
+}
+
+variable "AZURE_CLIENT_ID" {}
+variable "AZURE_SECRET" {}
+variable "AZURE_SUBSCRIPTION_ID" {}
+variable "AZURE_TENANT_ID" {}
+
+resource "azurerm_resource_group" "example" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.example.location
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
+}
 
-  storage_image_reference {
-    publisher = "Canonical"
+resource "azurerm_network_interface" "example" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "myVM"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  size                = "Standard_DS1_v2"
+  admin_username      = "azureuser"
+  disable_password_authentication = true
+
+  image_reference {
     offer     = "UbuntuServer"
+    publisher = "Canonical"
     sku       = "16.04-LTS"
     version   = "latest"
-  }
-
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "azureuser"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      path     = "/home/azureuser/.ssh/authorized_keys"
-      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFUeQxSYd+j62oq03FdqLdiXWduVDzcUbLDF8V+aBM9XXsYUpw0M9Xnc8Itxnz/Gua1YjQ+ZsF4hHJ0seA1"
-    }
-  }
-
-  provisioner "file" {
-    source      = "https://github.com/ajaykushwah17/Terraform-Ansible"
-    destination = "/tmp/playbook.yml"
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook /tmp/playbook.yml"
   }
 }
 
