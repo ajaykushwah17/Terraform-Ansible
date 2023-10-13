c707f323bb0ec7a61afbd4b8e4c1a72d0c7cd397
ajaykushwah17 <123883271+ajaykushwah17@users.noreply.github.com>
2023-10-13 13:49:39 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,74 +1,23 @@
-provider "azurerm" {
-  features {}
-}
+name: Create an Azure VM
+  hosts: localhost
+  tasks:
+    - name: Create a resource group
+      azure_rm_resourcegroup:
+        name: myResourceGroup
+        location: eastus
 
-resource "azurerm_resource_group" "example" {
-  name     = "myResourceGroup"
-  location = "eastus"
-}
+ 
 
-resource "azurerm_virtual_network" "example" {
-  name                = "myVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "myPublicIP"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-}
-
-resource "azurerm_network_security_group" "example" {
-  name     = "myNetworkSecurityGroup"
-  location = azurerm_resource_group.example.location
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "myNIC"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "myVM"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-  size                = "Standard_DS1_v2"
-  admin_username      = "azureuser"
-  disable_password_authentication = true
-  image_reference {
-    offer     = "CentOS"
-    publisher = "OpenLogic"
-    sku       = "7.5"
-    version   = "latest"
-  }
-}
+    - name: Create an Azure VM
+      azure_rm_virtualmachine:
+        resource_group: myResourceGroup
+        name: myVM
+        location: eastus
+        size: Standard_DS1_v2
+        admin_username: Subhojit_agreeya@outlook.com
+        admin_password: Subho_01
+        image:
+          offer: UbuntuServer
+          publisher: Canonical
+          sku: 18.04-LTS
+      register: vm
