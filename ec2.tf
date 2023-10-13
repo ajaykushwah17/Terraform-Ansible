9d10e92a3831cd6c85b101596c2abf1ebf066de3
ajaykushwah17 <123883271+ajaykushwah17@users.noreply.github.com>
2023-10-13 16:32:45 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -15,6 +15,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+client_id           = var.AZURE_CLIENT_ID
+  client_secret       = var.AZURE_SECRET
+  subscription_id     = var.AZURE_SUBSCRIPTION_ID
+  tenant_id           = var.AZURE_TENANT_ID
 }
 
 resource "azurerm_resource_group" "example" {
