7c193fb68526f01a1276e6c9601e911a698f1b20
ajaykushwah17 <123883271+ajaykushwah17@users.noreply.github.com>
2023-10-13 13:53:27 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,23 +1,53 @@
-name: Create an Azure VM
-  hosts: localhost
-  tasks:
-    - name: Create a resource group
-      azure_rm_resourcegroup:
-        name: myResourceGroup
-        location: eastus
+resource "azurerm_virtual_machine" "example" {
+  name                  = "myVM"
+  location              = "East US"
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  vm_size               = "Standard_DS1_v2"
 
- 
+  storage_os_disk {
+    name              = "myOsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
 
-    - name: Create an Azure VM
-      azure_rm_virtualmachine:
-        resource_group: myResourceGroup
-        name: myVM
-        location: eastus
-        size: Standard_DS1_v2
-        admin_username: Subhojit_agreeya@outlook.com
-        admin_password: Subho_01
-        image:
-          offer: UbuntuServer
-          publisher: Canonical
-          sku: 18.04-LTS
-      register: vm
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "azureuser"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path     = "/home/azureuser/.ssh/authorized_keys"
+      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFUeQxSYd+j62oq03FdqLdiXWduVDzcUbLDF8V+aBM9XXsYUpw0M9Xnc8Itxnz/Gua1YjQ+ZsF4hHJ0seA1"
+    }
+  }
+
+  provisioner "file" {
+    source      = "https://github.com/ajaykushwah17/Terraform-Ansible"
+    destination = "/tmp/playbook.yml"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /tmp/playbook.yml"
+  }
+}
+
+resource "null_resource" "ansible_trigger" {
+  triggers = {
+    vm_id = azurerm_virtual_machine.example.id
+  }
+
+  provisioner "local-exec" {
+    command = "echo Trigger Ansible here (replace this with your logic)"
+  }
+}
